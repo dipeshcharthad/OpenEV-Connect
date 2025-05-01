3c6b7aa587541da973f44426525b81106a4afab6
Dipesh Charthad <63149601+dipeshcharthad@users.noreply.github.com>
2025-05-01 18:13:25 +0530

    Update ev_simulator.py

--- sim/ev_simulator.py
+++ sim/ev_simulator.py
@@ -1,1 +1,11 @@
+import time
+import random
 
+while True:
+    data = {
+        "battery": round(48 + random.random(), 2),
+        "temperature": round(30 + random.random() * 10, 2),
+        "speed": round(40 + random.random() * 10, 2)
+    }
+    print(data)
+    time.sleep(1)
