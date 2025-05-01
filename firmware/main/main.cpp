a6648111c2cbe4d73bccbf27306f6422d2dd0eed
Dipesh Charthad <63149601+dipeshcharthad@users.noreply.github.com>
2025-05-01 18:09:35 +0530

    Update main.cpp

--- firmware/main/main.cpp
+++ firmware/main/main.cpp
@@ -1,1 +1,20 @@
+#include <Arduino.h>
+#include <BluetoothSerial.h>
+BluetoothSerial SerialBT;
 
+float batteryVoltage = 48.5;
+float motorTemp = 35.2;
+float speed = 42.7;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("OpenEV-ESP32");
+}
+
+void loop() {
+  String data = "{\"battery\":" + String(batteryVoltage) +
+                ",\"temperature\":" + String(motorTemp) +
+                ",\"speed\":" + String(speed) + "}";
+  SerialBT.println(data);
+  delay(1000);
+}
