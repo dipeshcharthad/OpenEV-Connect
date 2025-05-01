d4c779fff16d824bf07fe42fbf81db744b572bf7
Dipesh Charthad <63149601+dipeshcharthad@users.noreply.github.com>
2025-05-01 18:11:21 +0530

    Update App.js

--- app/react-native/App.js
+++ app/react-native/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import Dashboard from './components/Dashboard';
 
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Dashboard />
+    </SafeAreaView>
+  );
+};
+
+export default App;
